7b1def6c0de73669b2dd1a34e094874ade73695c
Nick <nickjpanella@gmail.com>
2023-11-11 23:12:21 -0500

    fix: add cursor pointer to box and flex components if onClick is defined

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -2,6 +2,11 @@ import { tags } from "../supercharge/supercharge";
 
 export const Box = tags.div({
   testid: ({ testid = null }) => `box${testid ? "-".concat(testid) : ""}`,
-})({
+})((props) => ({
   label: "box",
-});
+  ...(typeof props.onClick === "function"
+    ? {
+        cursor: "pointer",
+      }
+    : {}),
+}));
--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -13,16 +13,30 @@ export type FlexProps = {
 export const Flex = tags.div<FlexProps>({
   testid: ({ testid = null }) => `flex${testid ? "-".concat(testid) : ""}`,
   ignore: ["inline", "column", "reverse", "fill"],
-})(({ inline = false, column = false, reverse = false, fill = false }) => ({
-  label: "flex",
-  display: inline ? "inline-flex" : "flex",
+})(
+  ({
+    inline = false,
+    column = false,
+    reverse = false,
+    fill = false,
+    onClick,
+  }) => ({
+    label: "flex",
+    display: inline ? "inline-flex" : "flex",
 
-  flexDirection: csn(
-    { row: !column && !reverse },
-    { column: column && !reverse },
-    { "row-reverse": !column && reverse },
-    { "column-reverse": column && reverse }
-  ) as CSSProperties["flexDirection"],
+    flexDirection: csn(
+      { row: !column && !reverse },
+      { column: column && !reverse },
+      { "row-reverse": !column && reverse },
+      { "column-reverse": column && reverse }
+    ) as CSSProperties["flexDirection"],
 
-  flex: fill ? "1 1 auto" : undefined,
-}));
+    flex: fill ? "1 1 auto" : undefined,
+
+    ...(typeof onClick === "function"
+      ? {
+          cursor: "pointer",
+        }
+      : {}),
+  })
+);
